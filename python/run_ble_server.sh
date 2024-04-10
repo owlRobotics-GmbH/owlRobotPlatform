bf754bc9e6c34642eedca048f5a2ec845fa4c20a
greymfm <pi@testpi5.local>
2024-04-10 23:14:04 +0200

    update script

--- python/run_ble_server.sh
+++ python/run_ble_server.sh
@@ -14,13 +14,16 @@ python3 -c "import bumble"
 #echo $?
 if [[ "$?" -eq 1 ]]; then 
     echo "installing python3 packages..."    
-    pip3 install --break-system-packages bumble
-    pip3 install --break-system-packages python-can
+    # pip3 install --break-system-packages bumble==0.0.190
+    # pip3 install --break-system-packages bumble==4.3.1
+    pip3 install --break-system-packages bumble      # tested version: 0.0.190
+    pip3 install --break-system-packages python-can  # tested version: 4.3.1
 fi
 
 #apt install bluetooth pi-bluetooth bluez blueman
 
 echo "setup CAN interface..."
+echo "NOTE: you may have to edit boot config to enable CAN driver (see https://github.com/owlRobotics-GmbH/owlRobotPlatform)"
 ip link set can0 up type can bitrate 1000000
 
 echo "setup bluetooth interface..."
