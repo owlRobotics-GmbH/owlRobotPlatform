9abac48ac8e1d4f382a622e03c563d5b2d6c10f9
greymfm <pi@testpi5.local>
2024-04-10 18:05:32 +0200

    adjust script

--- python/run_ble_server.sh
+++ python/run_ble_server.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/bash 
 
-python -c "import bumble"
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (sudo):   sudo ./run_ble_server.sh"
+  exit
+fi
+
+if [ "$(which python3)" == "" ]; then
+    echo "installing python3..."
+    apt install python3
+fi
+
+python3 -c "import bumble"
 #echo $?
 if [[ "$?" -eq 1 ]]; then 
-    echo "installing python packages..."    
-    pip install --break-system-packages bumble
-    pip install --break-system-packages python-can
+    echo "installing python3 packages..."    
+    pip3 install --break-system-packages bumble
+    pip3 install --break-system-packages python-can
 fi
 
 #apt install bluetooth pi-bluetooth bluez blueman
@@ -21,5 +31,5 @@ hciconfig hci0 down
 
 
 echo "starting python app..."
-python ble_server.py
+python3 ble_server.py
 
