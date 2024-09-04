f6e4af61ed1f29bae70f403a39d5f39aea82cafc
greymfm <alex@grauonline.de>
2024-09-04 13:25:16 +0200

    can: add charger voltage

--- owlController_RP2040_00/Funkt.cpp
+++ owlController_RP2040_00/Funkt.cpp
@@ -96,7 +96,7 @@ float Funkt::getBatteryVoltage(){
 
 float Funkt::getChargerVoltage(){
   float adcVolt = ADC_read(0);
-  chargerVoltage = adcVolt * 10.0 / 90.0;  // U1/U2 = R1/R2   => U1=R1/R2*U2=10/290*adcVolt 
+  chargerVoltage = adcVolt * 10.0 / 1.0;  // U1/U2 = R1/R2   => U1=R1/R2*U2=10/290*adcVolt 
     #ifdef DEBUG
       Serial.print("  chgV: ");Serial.print(chargerVoltage);Serial.println("  mV ");
     #endif
@@ -129,6 +129,12 @@ void Funkt::extPieper(bool on_off)
     digitalWrite(PIEPER_Pin,on_off);
    }
     
+
+  // chn_select :  
+  //    110         anaMUX_Adr0 : 0
+  //                anaMUX_Adr1 : 1
+  //                anaMUX_Adr2 : 1
+
   void Funkt::anaMux(byte chn_select)
    {
      digitalWrite(anaMUX_Adr0,chn_select & 0b00000001);
