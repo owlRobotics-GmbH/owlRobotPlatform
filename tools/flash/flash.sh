269767b3c08a8e1ef801371d65d346ce531d27e4
greymfm <alex@grauonline.de>
2025-03-08 16:34:31 +0100

    update flash.sh

--- tools/flash/flash.sh
+++ tools/flash/flash.sh
@@ -2,8 +2,9 @@
 
 # script to flash 'owlController firmware' onto  RP2040 (Raspberry Pico)  
 
-# 1. connect RP2040 USB to RaspberryPI5
-# 2. run this script :  ./flash.sh
+# 1. connect owlController's RP2040 USB to owlController's RaspberryPI5
+# 2. press button 'Reset RP2040' on owlController PCB 
+# 3. run this script :  ./flash.sh
 
 
 # example output:
@@ -20,6 +21,13 @@
 #   flash successful - black magic happened ~\(o.O~\)
 #   pi@testpi5:~/owlRobotPlatform/tools/flash $
 
+
+
+echo "This script will flash 'owlController firmware' onto  RP2040 (Raspberry Pico)"
+echo "1. connect owlController's RP2040 USB to owlController's RaspberryPI5"
+echo "2. press button 'Reset RP2040' on owlController PCB"
+
+pause 
 
 # allow user to access /dev/ttyACM0
 sudo usermod -a -G dialout $USER
