89af43429c5146b602e4d9e73a4f52c9a7f377b8
AlexanderG <alexandergrau@gmx.de>
2025-03-08 17:15:39 +0100

    update flash scripts

--- tools/flash/flash.sh
+++ tools/flash/flash.sh
@@ -2,31 +2,15 @@
 
 # script to flash 'owlController firmware' onto  RP2040 (Raspberry Pico)  
 
-# 1. connect owlController's RP2040 USB to owlController's RaspberryPI5
-# 2. press button 'Reset RP2040' on owlController PCB 
-# 3. run this script :  ./flash.sh
-
-
-# example output:
-#   pi@testpi5:~/owlRobotPlatform/tools/flash $ ./flash.sh 
-#   waiting for pico to mount
-#   deleted mass storage relics (if existing): /
-#   resetting pico
-#   .
-#   .
-#   .
-#   copy flash-file to pico...
-#   './owlController_RP2040_00.ino.uf2' -> '/media/pi/RPI-RP2/owlController_RP2040_00.ino.uf2'
-#   .
-#   flash successful - black magic happened ~\(o.O~\)
-#   pi@testpi5:~/owlRobotPlatform/tools/flash $
-
-
 
 echo "This script will flash 'owlController firmware' onto  RP2040 (Raspberry Pico)"
-echo "1. connect owlController's RP2040 USB to owlController's RaspberryPI5"
-echo "2. press button 'Reset RP2040' on owlController PCB"
-
+echo 
+echo "1. Disconnect owlController's RP2040 USB"
+echo "2. Press and hold button 'Boot Set' on owlController PCB"
+echo "3. Press button 'Reset RP2040' on owlController PCB" 
+echo "4. Release button 'Boot Set'"
+echo "5. Connect owlController's RP2040 USB to owlController's RaspberryPI5"
+echo 
 read -p "Press ENTER to continue..."
 
 # allow user to access /dev/ttyACM0
