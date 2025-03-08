f8490cb21ebfe34c427aa3054eaffec2256aacf5
greymfm <alex@grauonline.de>
2025-03-08 13:44:12 +0100

    update flash.sh

--- tools/flash/flash.sh
+++ tools/flash/flash.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# script to flash 'owlController firmware' onto  RP2040 (Raspberry Pico)  
+
+# 1. connect RP2040 USB to RaspberryPI5
+# 2. run this script 
+
 ./picoFlashTool_Linux ttyACM0
